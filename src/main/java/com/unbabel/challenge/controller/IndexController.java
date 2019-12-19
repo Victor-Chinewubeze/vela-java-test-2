12131113dac9f0a6c98cea2d638147767e576e00
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-12-19 17:16:47 +0100

    Create endpoint for translating a given message

--- src/main/java/com/unbabel/challenge/controller/IndexController.java
+++ src/main/java/com/unbabel/challenge/controller/IndexController.java
@@ -1,11 +1,15 @@
 package com.unbabel.challenge.controller;
 
+import com.unbabel.challenge.model.Message;
 import com.unbabel.challenge.repository.MessageRepository;
+import com.unbabel.challenge.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IndexController {
@@ -13,12 +17,14 @@ public class IndexController {
     @Autowired
     MessageRepository messageRepository;
 
+    @Autowired
+    MessageService messageService;
+
     @Value("${java.challenge.company}")
     private String company; // Reads this value from Spring properties file
 
     /**
-     * Accepts get requests to the "/" url, generates random messages
-     * and renders them in thymeleaf template (index.html).
+     * Renders the homepage
      * @param model inject objects into thymeleaf template
      * @return generated html page using thymeleaf
      */
@@ -30,5 +36,19 @@ public class IndexController {
     }
 
 
+    /**
+     * Post request for translating a given message
+     *
+     * @param message
+     */
+    @PostMapping("/")
+    public String translateGivenMessage(@ModelAttribute Message message) {
+        message.setSource_language("en");
+        message.setText_format("text");
+        messageService.translate(message);
+
+        return "redirect:/";
+
+    }
 
 }
