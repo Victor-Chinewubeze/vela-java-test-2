b2bc80fc84c9dc67bad14b38724d9f2355246ced
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2019-12-19 16:28:47 +0100

    Update Message Service interface

--- src/main/java/com/unbabel/challenge/services/MessageService.java
+++ src/main/java/com/unbabel/challenge/services/MessageService.java
@@ -2,8 +2,16 @@ package com.unbabel.challenge.services;
 
 import com.unbabel.challenge.model.Message;
 
+import java.util.Set;
+
 public interface MessageService {
 
     Message translate(Message message);
+
+    Message getStatus(Message message);
+
+    void saveMessage(Message message);
+
+    Set<Message> getMessages();
 
 }
